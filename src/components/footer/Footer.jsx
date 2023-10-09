253d43fb32a1cc331335ab28431b862cbda01d22
SaukiFutaki <128306993+SaukiFutaki@users.noreply.github.com>
2023-10-09 20:14:51 +0700

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { CalendarDaysIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
+
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import MyContext from "../../context/data/myContext";
