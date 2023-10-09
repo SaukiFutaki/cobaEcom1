893d5b68840b64b6c0f9d13fd2f9cdcae5e80eab
SaukiFutaki <128306993+SaukiFutaki@users.noreply.github.com>
2023-10-09 20:11:18 +0700

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,10 @@
 import { CalendarDaysIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import myContext from "../../context/data/myContext";
+import MyContext from "../../context/data/myContext";
 
 export default function Footer() {
-  const context = useContext(myContext);
+  const context = useContext(MyContext);
   const { toggleMode, mode } = context;
   return (
     <footer
